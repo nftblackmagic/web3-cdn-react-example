34cbf74a0a23958ac41d5af33398a33643122c15
An <anzhanghust@gmail.com>
2022-12-22 18:36:15 -0800

    first version

--- src/App.js
+++ src/App.js
@@ -2,21 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log("App.js");
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Catchon CDN React example</h1>
+        <h3>{"<script> has been insert in public/index.html"}</h3>
+        <button connect-button="0">
+          Connect wallet
+        </button>
+        <div className='row'>
+          <h3>Name: </h3>
+          <h3 function-name="name">
+            Name
+          </h3>
+        </div>
+        <div className='row'>
+          <h3>balanceOf: </h3>
+          <h3 function-name="balanceOf" function-name-args={'["0x4FCEBB7D6688e5Ac0b1Ed543F25f2601CBdb1007"]'}>
+            balanceOf
+          </h3>
+        </div>
+        <div className='row'>
+          <h3>isMintOn: </h3>
+          <h3 function-name="isPublicSaleOn">
+            isMintOn
+          </h3>
+        </div>
+        <button function-name="mint">
+          Mint
+        </button>
       </header>
     </div>
   );
