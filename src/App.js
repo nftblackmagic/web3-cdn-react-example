5c4c6b09c41080b8163b985d964774f65fbc62d8
An <anzhanghust@gmail.com>
2022-12-23 00:54:32 -0800

    fix misc error

--- src/App.js
+++ src/App.js
@@ -6,21 +6,22 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Catchon CDN React example</h1>
+        <h1>Catchon CDN React example (React 17 only)</h1>
         <h3>{"<script> has been insert in public/index.html"}</h3>
+        <h3 style={{ color: "red" }}> {"This app is required to run on goerli testnet"}</h3>
         <button connect-button="0">
           Connect wallet
         </button>
         <div className='row'>
-          <h3>Name: </h3>
+          <h3>NFT Name: </h3>
           <h3 function-name="name">
-            Name
+            Not loaded yet
           </h3>
         </div>
         <div className='row'>
-          <h3>balanceOf: </h3>
-          <h3 function-name="balanceOf" function-name-args={'["0x4FCEBB7D6688e5Ac0b1Ed543F25f2601CBdb1007"]'}>
-            balanceOf
+          <h3>NFT minted: </h3>
+          <h3 function-name="totalSupply">
+            Not loaded yet
           </h3>
         </div>
         <div className='row'>
@@ -33,7 +34,7 @@ function App() {
           Mint
         </button>
       </header>
-    </div>
+    </div >
   );
 }
 
